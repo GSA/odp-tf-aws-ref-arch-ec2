cc7985bf91312e798ca311c18d205aea82d59939
RobertJLupinek <9854828608@GSA.GOV>
2019-12-06 16:46:27 -0500

    Update - Making example work for validate.

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   source = "../"
-  aws_region = "${var.env}"
+  aws_region = "${var.aws_region}"
   subnet_private_1a_id = "SUBNET ID"
   subnet_private_1b_id = "SUBNET ID"
   subnet_public_1a_id = "SUBNET ID"
@@ -10,7 +10,7 @@ module "ec2" {
   subnet_public_1a_cidr = "10.1.0.1/24"
   subnet_public_1b_cidr = "10.1.0.2/24"  
   vpc_id = "VPC ID"
-  jump_host_allowed_cidr = "172.20.0.1/24"
+  jump_host_allowed_cidr = ["172.20.0.1/24"]
   aws_key_name = "${var.aws_key_name}"
-  application_allowed_cidr = "172.20.0.2/24"
+  application_allowed_cidr = ["172.20.0.2/24"]
 }
