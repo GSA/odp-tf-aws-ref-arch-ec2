75767338bf7d5859ec6afbf3fd0694b3d1064669
RobertJLupinek <9854828608@GSA.GOV>
2019-12-09 16:32:24 -0500

    (fix) Correcting issues with AMI lookup.

--- variables.tf
+++ variables.tf
@@ -66,11 +66,11 @@ variable "aws_key_name" {
 }
 
 variable "tag_for_ami_name_jump" {
-  default = ["Packer-Jump"]
+  default = ["ubuntu-xenial-16.04-amd64-server-20191114"]
 }
 
 variable "tag_for_ami_name_private" {
-  default = ["Packer-Private"]
+  default = ["ubuntu-xenial-16.04-amd64-server-20191114"]
 }
 
 variable "jump_host_allowed_cidr_list" {
